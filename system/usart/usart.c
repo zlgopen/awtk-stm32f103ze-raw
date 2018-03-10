3baba1f3b0bc189296beb3c47586d28bb50d39d1
xianjimli <xianjimli@hotmail.com>
2018-03-11 07:52:50 +0800

    update project to support jtag debug

--- system/usart/usart.c
+++ system/usart/usart.c
@@ -27,8 +27,8 @@ _sys_exit(int x)
 //重定义fputc函数 
 int fputc(int ch, FILE *f)
 {      
-	while((USART1->SR&0X40)==0);//循环发送,直到发送完毕   
-    USART1->DR = (u8) ch;      
+	//while((USART1->SR&0X40)==0);//循环发送,直到发送完毕   
+  //  USART1->DR = (u8) ch;      
 	return ch;
 }
 #endif 
