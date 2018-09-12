61f4058d55159c1ddfe55696ba3c4d8f0ca54dc8
xianjimli <xianjimli@gmail.com>
2018-09-12 11:41:07 +0800

    add awtk_config.h

---
+++ awtk-port/awtk_config.h
@@ -0,0 +1,49 @@
+
+/**
+ * File:   awtk_config.h
+ * Author: AWTK Develop Team
+ * Brief:  config
+ *
+ * Copyright (c) 2018 - 2018  Guangzhou ZHIYUAN Electronics Co.,Ltd.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * License file for more details.
+ *
+ */
+
+/**
+ * History:
+ * ================================================================
+ * 2018-09-12 Li XianJing <xianjimli@hotmail.com> created
+ *
+ */
+
+#ifndef AWTK_CONFIG_H
+#define AWTK_CONFIG_H
+
+/**
+ * 嵌入式系统有自己的main函数时，请定义本宏。
+ *
+ */
+#define USE_GUI_MAIN 1
+
+/**
+ * 如果需要支持预先解码的图片，请定义本宏。一般只在RAM极小时，才启用本宏。
+ */
+#define WITH_BITMAP_FONT 1
+
+/**
+ * 如果不需输入法，请定义本宏
+ *
+ */
+#define WITH_NULL_IM 1
+
+/**
+ * 如果支持极速模式，请定义本宏。极速模式不支持控件透明半透明效果，只有在CPU配置极低时启用。
+ *
+ */
+#define USE_FAST_MODE 1
+
+#endif/*AWTK_CONFIG_H*/
