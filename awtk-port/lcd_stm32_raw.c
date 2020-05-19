8ce23d246ebeb46b3da5abaf5b4265140fae18eb
lixianjing <xianjimli@hotmail.com>
2020-05-19 16:46:37 +0800

    fix for transparent lcd changes

--- awtk-port/lcd_stm32_raw.c
+++ awtk-port/lcd_stm32_raw.c
@@ -28,8 +28,10 @@ typedef uint16_t pixel_t;
 #define LCD_FORMAT BITMAP_FMT_BGR565
 #define pixel_from_rgb(r, g, b)                                                \
   ((((r) >> 3) << 11) | (((g) >> 2) << 5) | ((b) >> 3))
+#define pixel_from_rgba(r, g, b, a)                                            \
+  ((((r) >> 3) << 11) | (((g) >> 2) << 5) | ((b) >> 3))
 #define pixel_to_rgba(p)                                                       \
-  { (0xff & ((p >> 11) << 3)), (0xff & ((p >> 5) << 2)), (0xff & (p << 3)) }
+  { (0xff & ((p >> 11) << 3)), (0xff & ((p >> 5) << 2)), (0xff & (p << 3)), 0xff }
 
 #define set_window_func LCD_Set_Window
 #define write_data_func LCD_WriteData_Color
